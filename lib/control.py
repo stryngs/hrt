895cad85297aa481d5ffcb1555a98ceda7a22184
stryngs <support@configitnow.com>
2023-06-11 23:56:41 -0400

    rx and tx both working now, but still failing to switch

--- lib/control.py
+++ lib/control.py
@@ -2,11 +2,11 @@ from shlex import split
 import fcntl, subprocess, time
 
 class Control(object):
-    
+
     def __init__(self, **kwargs):
         self.rx = kwargs.get('rx')
         self.tx = kwargs.get('tx')
-        
+
 
     def kill(self, proc):
         """Reset the hackRF connection"""
@@ -19,8 +19,8 @@ class Control(object):
         s1 = subprocess.Popen(split('lsusb'), stdout = subprocess.PIPE)
         s2 = subprocess.Popen(split('grep OpenMoko'), stdin = s1.stdout, stdout = subprocess.PIPE)
         oPut = s2.communicate()[0].split()
-        bus = oPut[1]
-        dev = oPut[3].split(':')[0]
+        bus = oPut[1].decode()
+        dev = oPut[3].decode().split(':')[0]
         hackRF = '/dev/bus/usb/{0}/{1}'.format(bus, dev)
         USBDEVFS_RESET = ord('U') << (4*2) | 20
         with open(hackRF, 'w') as kOption:
